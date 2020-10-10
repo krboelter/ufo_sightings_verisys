e857c32bc2b5bd625761569a0cc9de1048baac4b
Kenneth Boelter <kennethboelter@pop-os.localdomain>
2020-10-10 14:07:25 -0600

    created schema, add sighting, get all sightings

---
+++ backend/migrations/20201010134356_sightings.js
@@ -0,0 +1,22 @@
+exports.up = async function(knex) {
+    await knex.schema.createTable('sightings', table => {
+        table.increments()
+        table.date('sighting_date')
+            .notNullable()
+        table.string('shape')
+            .notNullable()
+        table.integer('duration')
+            .notNullable()
+        table.string('comments')
+        table.string('city')
+        table.string('state')
+        table.integer('latitude')
+            .notNullable()
+        table.integer('longitude')
+            .notNullable()
+    })
+};
+
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists
+};
